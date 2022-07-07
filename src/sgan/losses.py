dd23ddb2c302f910a2220f9ac566061c9eef9b67
fedesigno <f.signoretta@campus.unimib.it>
2022-07-07 14:43:29 +0200

    code cleaning

--- src/sgan/losses.py
+++ src/sgan/losses.py
@@ -135,3 +135,34 @@ def final_displacement_error(
         return loss
     else:
         return torch.sum(loss)
+
+
+def cal_l2_losses(
+    pred_traj_gt, pred_traj_gt_rel, pred_traj_fake, pred_traj_fake_rel,
+    loss_mask
+):
+    g_l2_loss_abs = l2_loss(
+        pred_traj_fake, pred_traj_gt, loss_mask, mode='sum'
+    )
+    g_l2_loss_rel = l2_loss(
+        pred_traj_fake_rel, pred_traj_gt_rel, loss_mask, mode='sum'
+    )
+    return g_l2_loss_abs, g_l2_loss_rel
+
+def cal_ade(pred_traj_gt, pred_traj_fake, linear_ped, non_linear_ped):
+    ade = displacement_error(pred_traj_fake, pred_traj_gt)
+    ade_l = displacement_error(pred_traj_fake, pred_traj_gt, linear_ped)
+    ade_nl = displacement_error(pred_traj_fake, pred_traj_gt, non_linear_ped)
+    return ade, ade_l, ade_nl
+
+def cal_fde(
+    pred_traj_gt, pred_traj_fake, linear_ped, non_linear_ped
+):
+    fde = final_displacement_error(pred_traj_fake[-1], pred_traj_gt[-1])
+    fde_l = final_displacement_error(
+        pred_traj_fake[-1], pred_traj_gt[-1], linear_ped
+    )
+    fde_nl = final_displacement_error(
+        pred_traj_fake[-1], pred_traj_gt[-1], non_linear_ped
+    )
+    return fde, fde_l, fde_nl
